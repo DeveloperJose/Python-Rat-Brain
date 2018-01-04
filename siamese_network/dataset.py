26cf3d8f05ad7a2469e455db3639e17fb2ab0151
Jose Perez <josegperez@mail.com>
2018-01-03 17:32:02 -0700

    Updated dataset

--- siamese_network/dataset.py
+++ siamese_network/dataset.py
@@ -1,12 +1,4 @@
 import numpy as np
-from sklearn.feature_selection import SelectFromModel
-from sklearn.linear_model import LassoCV
-from sklearn.model_selection import GridSearchCV
-from sklearn.preprocessing import RobustScaler
-from keras import backend as K
-from keras.models import Sequential
-from keras.layers import Dense, InputLayer, GaussianNoise
-from keras.wrappers.scikit_learn import KerasRegressor
 
 def normalize(dataset):
     mean = np.mean(dataset)
